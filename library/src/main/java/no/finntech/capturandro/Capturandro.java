e63363a28eade6aaa29c5edbd221569d3d455e4d
Balder Mørk <balder.mork@finn.no>
2020-08-31 08:58:18 +0200

    Task/android 11 fixes (#13)
    * Update build scripts for API 30 and AndroidX
    
    * Update code to more modern standards, check for missing external cache
    
    - Set language level to Java 8
    - Use AndroidX
    - Use RxJava instead of deprecated AsyncTask
    - Raise minSdkVersion to 21
    - Remove unneccessary WRITE_EXTERNAL_STORAGE permission
    - If external cache is unavailable, fall back to internal
    - Fix typo in filename filter, so we actually clean up
    
    * Bump version number
    
    * Accept license for android tools
    
    * Try again to accept license for android tools
    
    * Fix typo
    
    * PR Feedback

--- library/src/main/java/no/finntech/capturandro/Capturandro.java
+++ library/src/main/java/no/finntech/capturandro/Capturandro.java
@@ -7,22 +7,22 @@ import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcel;
 import android.os.Parcelable;
-import android.os.Process;
 import android.provider.MediaStore;
-import android.support.annotation.RequiresPermission;
-import android.support.v4.content.FileProvider;
+
+import androidx.annotation.RequiresPermission;
+import androidx.core.content.FileProvider;
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.util.concurrent.Executors;
 
+import io.reactivex.Completable;
 import io.reactivex.Observable;
 import io.reactivex.Scheduler;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 /*
@@ -30,6 +30,7 @@ import io.reactivex.schedulers.Schedulers;
 */
 public class Capturandro {
     private static final String KEY = "CAPTURANDO_STATE";
+    protected static final String TEMPFILE_PREFIX = "capturandro-";
     public static final int DEFAULT_STORED_IMAGE_COMPRESSION_PERCENT = 75;
 
     private final CapturandoCallback callback;
@@ -37,7 +38,7 @@ public class Capturandro {
     private CapturandoState state = null;
     private static boolean initialStartup = true;
     private static String fileProviderAuthority;
-    private AsyncTask<Void, Void, Void> deleteTask;
+    private Disposable deleteTask;
 
     public Capturandro(Context context, CapturandoCallback callback, String fileProviderAuthority) {
         super();
@@ -56,8 +57,8 @@ public class Capturandro {
     }
 
     public void onDestroy() {
-        if (deleteTask != null) {
-            deleteTask.cancel(true);
+        if (deleteTask != null && !deleteTask.isDisposed()) {
+            deleteTask.dispose();
         }
     }
 
@@ -65,7 +66,7 @@ public class Capturandro {
         outState.putParcelable(KEY, state);
     }
 
-    @RequiresPermission(allOf = { Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA })
+    @RequiresPermission(Manifest.permission.CAMERA)
     public void importImageFromCamera(final Activity activity, final int requestCode) {
         importImageFromCamera(activity, requestCode, -1);
     }
@@ -89,7 +90,7 @@ public class Capturandro {
      * When executing subscribe on the Obserable<Uri> image processing may take place (on a seperate thread).
      * Image processing is done on a single background thread to prevent oom
      */
-    @RequiresPermission(allOf = { Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA })
+    @RequiresPermission(Manifest.permission.CAMERA)
     public void importImageFromCamera(final Activity activity, final int requestCode, final int longestSide) {
         String filename = BitmapUtil.getUniqueFilename(activity);
         state = new CameraState(longestSide, filename);
@@ -110,12 +111,10 @@ public class Capturandro {
 
     }
 
-    @RequiresPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
     public void importImageFromGallery(Activity activity, int requestCode) {
         importImageFromGallery(activity, requestCode, -1);
     }
 
-    @RequiresPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
     public void importImageFromGallery(Activity activity, int requestCode, int longestSide) {
         importImageFromGallery(activity, requestCode, longestSide, true);
     }
@@ -126,13 +125,12 @@ public class Capturandro {
      * When executing subscribe on the Obserable<Uri> image processing may take place (on a seperate thread).
      * * Image processing is done on a single background thread to prevent oom
      */
-    @RequiresPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
     public void importImageFromGallery(final Activity activity, final int requestCode, final int longestSide, final boolean multiselect) {
         state = new GalleryState(longestSide, multiselect);
         Intent intent;
         try {
             intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            if (Build.VERSION.SDK_INT >= 18 && multiselect) {
+            if (multiselect) {
                 intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
             }
             activity.startActivityForResult(intent, requestCode);
@@ -140,7 +138,7 @@ public class Capturandro {
             intent = new Intent(Intent.ACTION_GET_CONTENT);
             intent.setType("image/*");
             intent.addCategory(Intent.CATEGORY_OPENABLE);
-            if (Build.VERSION.SDK_INT >= 18 && multiselect) {
+            if (multiselect) {
                 intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
             }
             activity.startActivityForResult(intent, requestCode);
@@ -158,15 +156,13 @@ public class Capturandro {
                 GalleryState state = (GalleryState) this.state;
                 ImportHandler importHandler = new ImportHandler(activity, state.longestSide);
                 if (intent != null) {
-                    if (Build.VERSION.SDK_INT >= 18) {
-                        ClipData clipData = intent.getClipData();
-                        if (clipData != null && clipData.getItemCount() > 0) {
-                            for (int i = 0; i < clipData.getItemCount(); i++) {
-                                Uri uri = clipData.getItemAt(i).getUri();
-                                callback.onImport(requestCode, importHandler.gallery(scheduler, uri));
-                            }
-                            return;
+                    ClipData clipData = intent.getClipData();
+                    if (clipData != null && clipData.getItemCount() > 0) {
+                        for (int i = 0; i < clipData.getItemCount(); i++) {
+                            Uri uri = clipData.getItemAt(i).getUri();
+                            callback.onImport(requestCode, importHandler.gallery(scheduler, uri));
                         }
+                        return;
                     }
                     Uri selectedImage = intent.getData();
                     callback.onImport(requestCode, importHandler.gallery(scheduler, selectedImage));
@@ -184,35 +180,29 @@ public class Capturandro {
     }
 
     private void clearAllCachedBitmaps(Context context) {
-        File externalCacheDir = context.getExternalCacheDir();
-        if (externalCacheDir != null) {
-            final String cachePath = externalCacheDir.getAbsolutePath();
-            deleteTask = new AsyncTask<Void, Void, Void>() {
-                @Override
-                protected Void doInBackground(Void... params) {
-                    Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
-                    File[] files = new File(cachePath).listFiles(new FilenameFilter() {
-                        @Override
-                        public boolean accept(File dir, String filename) {
-                            return filename.startsWith("capturando-") && filename.endsWith(".jpg");
-                        }
-                    });
-                    // If READ_EXTERNAL_STORAGE permission isn't granted, files might be null
-                    if (files != null) {
-                        for (File file : files) {
-                            if (isCancelled()) {
-                                return null;
-                            }
-                            try {
-                                file.delete();
-                            } catch (Exception e) {
-                                e.printStackTrace();
+        final File cacheDir = BitmapUtil.getCacheDir(context);
+        if (cacheDir != null) {
+            deleteTask = Completable.create(emitter -> {
+                        File[] files = cacheDir.listFiles((dir, filename) ->
+                                filename.startsWith(TEMPFILE_PREFIX) && filename.endsWith(".jpg")
+                        );
+                        if (files != null) {
+                            for (File file : files) {
+                                if (emitter.isDisposed()) {
+                                    return;
+                                }
+                                try {
+                                    file.delete();
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
                             }
                         }
+                        emitter.onComplete();
                     }
-                    return null;
-                }
-            }.execute();
+            )
+                    .subscribeOn(Schedulers.io())
+                    .subscribe();
         }
     }
 
